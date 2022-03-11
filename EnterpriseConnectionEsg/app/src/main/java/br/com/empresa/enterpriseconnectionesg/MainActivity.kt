48a12c3a4fb08c6c818a665a02068982fe60e8b5
mnnobre <mn.nobre@outlook.com>
2022-03-11 01:15:14 -0300

    add register scren

---
+++ EnterpriseConnectionEsg/app/src/main/java/br/com/empresa/enterpriseconnectionesg/MainActivity.kt
@@ -0,0 +1,15 @@
+package br.com.empresa.enterpriseconnectionesg
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+    }
+
+
+}
